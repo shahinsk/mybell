b4dec033a635aff882df45b67f72582b8b43dc9c
shahinsk <53890562+shahinsk@users.noreply.github.com>
2026-03-02 23:21:33 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+
+self.addEventListener('push', e => {
+  const data = e.data ? e.data.json() : { title: '🔔 Bell Trigger', body: 'Someone rang your bell!' };
+  e.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🔔</text></svg>',
+      vibrate: [200, 100, 200, 100, 200],
+      tag: 'bell-trigger',
+      renotify: true,
+    })
+  );
+});
+
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  e.waitUntil(clients.openWindow('/'));
+});
